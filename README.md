1c40a323cd58dae8b8d095cd51e40923c130390f
master <jgy1357@naver.com>
2019-01-14 22:22:46 +0900

    docs: 2주차 pdf, 과제 업로드

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# js-study
+# 자바스크립트 스터디 저장소
+
+> [w3shcool](https://w3schools.com/js/default.asp)
+## 1주차
+- [pdf](https://github.com/js-study-master/js-study/tree/master/pdf/1%EC%A3%BC%EC%B0%A8_note.pdf)
+
+## 2주차
+- [pdf](https://github.com/js-study-master/js-study/tree/master/pdf/2%EC%A3%BC%EC%B0%A8_note.pdf)
+- [과제](https://github.com/js-study-master/js-study/tree/master/example/#1)
---
+++ examples/#1/calculator.js
@@ -0,0 +1,34 @@
+function onClick() {
+  var sel = document.getElementById('option'),
+    option = sel.value;     // options에는 선택한 옵션이 저장된다.
+
+  var result = document.getElementById('result'), // result는 결과를 보여줄 곳
+    input1 = document.getElementById('input1'),   // input1은 첫번째 숫자 값이 저장
+    input2 = document.getElementById('input2');   // input2은 두번째 숫자 값이 저장
+
+  /*
+   * option값에 따라 적절한 함수를 호출하여 계산한 값을 result로 반환하라
+   * hint: input의 값은 input.value를 호출하여 얻을 수 있다.
+   * input.value는 string으로 반환되기 때문에 계산을 위해선 형 변환이 필요하다. (구글에 javascript type casting 혹은 자바스크립트 형변환 검색)
+   *
+   * result.innerHTML에 계산값을 넣으면 출력된다. (ex. result.innerHTML = 결과)
+   */
+  
+   // 이곳에 코드를 작성하세요.
+}
+
+function addition(a, b) {
+  // TO-DO
+}
+
+function subtraction(a, b) {
+  // TO-DO
+}
+
+function multiplication(a, b) {
+  // TO-DO
+}
+
+function division(a, b) {
+  // TO-DO
+}
